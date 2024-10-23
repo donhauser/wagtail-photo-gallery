65fefd7576bb1045e6f835a3f6989d2639454aaa
Jonas Donhauser <donhauserjonas@googlemail.com>
2024-10-23 17:00:40 +0200

    updated photoswipe to v5

---
+++ wagtail_photo_gallery/static/wagtail_photo_gallery/js/init-photoswipe.js
@@ -0,0 +1,10 @@
+﻿import PhotoSwipeLightbox from 'https://cdn.jsdelivr.net/npm/photoswipe@5.4.4/dist/photoswipe-lightbox.esm.min.js';
+import PhotoSwipe from 'https://cdn.jsdelivr.net/npm/photoswipe@5.4.4/dist/photoswipe.esm.min.js';
+
+const lightbox = new PhotoSwipeLightbox({
+  gallery: '.photo-gallery-container-images',
+  children: 'a',
+  pswpModule: PhotoSwipe
+});
+
+lightbox.init();
